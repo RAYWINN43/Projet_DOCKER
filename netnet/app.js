9de49a79e0830b4c5ec286ec2d148e2b5ae1cf15
Snowlly <evanmartin2001@gmail.com>
2026-01-20 16:24:32 +0100

    MAJ : BDD

--- netnet/app.js
+++ netnet/app.js
@@ -1,6 +1,8 @@
+require("dotenv").config(); // ⬅️ TOUJOURS en premier
+
 const express = require("express");
 const mongoose = require("mongoose");
-const filmRoutes = require("./routes/film");
+const filmRoutes = require("./src/routes/film");
 
 const app = express();
 app.use(express.json());
@@ -8,10 +10,11 @@ app.use(express.json());
 // Routes
 app.use("/api/films", filmRoutes);
 
-// Mongo (exemple)
+// Mongo
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Mongo connecté"))
   .catch((err) => console.error("Mongo error:", err));
 
-app.listen(3000, () => console.log("API up on :3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`API up on :${PORT}`));
